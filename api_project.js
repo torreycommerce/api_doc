22e9639d5a087754b147485a671843c5d048c9a0
Brian Ly <brianly@Brians-Mac-mini.local>
2016-01-21 14:21:05 -0800

    updated api docs 2.0.1

---
+++ api_project.js
@@ -0,0 +1,16 @@
+define({
+  "name": "Acenda",
+  "version": "2.0.1",
+  "description": "Acenda REST API Documentation",
+  "title": "Acenda API",
+  "url": "https://demo.acenda.com",
+  "generate": "apidoc -f \"(.+)Controller(.*).php\" -i ./ -o ./bones/doc/public --verbose",
+  "sampleUrl": false,
+  "apidoc": "0.2.0",
+  "generator": {
+    "name": "apidoc",
+    "time": "2016-01-21T22:19:58.117Z",
+    "url": "http://apidocjs.com",
+    "version": "0.14.0"
+  }
+});
